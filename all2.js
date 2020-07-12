ef415cd3b54330dce3475ccbf63f2d347d1d642c
plsmeghana <pmeghana99@gmail.com>
2020-07-12 19:50:21 +0530

    TASK4 COMPLETED

--- all2.js
+++ all2.js
@@ -146,3 +146,127 @@ function anagram() {
     document.write("anagram");
     return true;
 }
+function random() {
+    num1 = Math.floor(Math.random() * 1000) + 1;
+    document.getElementById("p").value = num1;
+    alert(num1);
+    num2 = Math.floor(Math.random() * 1000) + 1;
+    document.getElementById("r").value = num2;
+    alert(num2);
+    mod1 = num1 % 10;
+    mod2 = num2 % 10;
+    alert("mod1 is" + mod1);
+    alert("mod2 is" + mod2);
+    if (mod1 == '1') {
+        object1 = "1";
+        alert("object1 is" + object1);
+    } else if (mod1 == '2') {
+        object1 = "1";
+        alert("object1 is" + object1);
+    } else if (mod1 == '3') {
+        object1 = "1";
+        alert("object1 is" + object1);
+    } else if (mod1 == '4') {
+        object1 = "2";
+        alert("object1 is" + object1);
+    } else if (mod1 == '5') {
+        object1 = "2";
+        alert("object1 is" + object1);
+    } else if (mod1 == '6') {
+        object1 = "2";
+        alert("object1 is" + object1);
+    } else if (mod1 == '7') {
+        object1 = "3";
+        alert("object1 is" + object1);
+    } else if (mod1 == '8') {
+        object1 = "3";
+        alert("object1 is" + object1);
+    } else if (mod1 == '9') {
+        object1 = "3";
+        alert("object1 is " + object1);
+    } else {
+        object1 = "3";
+        alert("object1 is " + object1);
+    }
+    if (mod2 == '1') {
+        object2 = "1";
+        alert("object2 is " + object2);
+    } else if (mod2 == '2') {
+        object2 = "1";
+        alert("object2 is " + object2);
+    } else if (mod2 == '3') {
+        object2 = "1";
+        alert("object2 is " + object2);
+    } else if (mod2 == '4') {
+        object2 = "2";
+        alert("object2 is " + object2);
+    } else if (mod2 == '5') {
+        object2 = "2";
+        alert("object2 is " + object2);
+    } else if (mod2 == '6') {
+        object2 = "2";
+        alert("object2 is " + object2);
+    } else if (mod2 == '7') {
+        object2 = "3";
+        alert("object2 is " + object2);
+    } else if (mod2 == '8') {
+        object2 = "3";
+        alert("object2 is " + object2);
+    } else if (mod2 == '9') {
+        object2 = "3";
+        alert("object2 is " + object2);
+    } else {
+        object2 = "3";
+        alert("object2 is " + object2);
+    }
+    switch (object1) {
+        case '1':
+            object1 = "human";
+            alert("human");
+            break;
+        case '2':
+            object1 = "cockroch";
+            alert("cockroch");
+            break;
+        case '3':
+            object1 = "nuclear bomb";
+            alert("nuclear bomb");
+            break;
+    }
+    switch (object2) {
+        case '1':
+            object2 = "human";
+            alert("human");
+            break;
+        case '2':
+            object2 = "cockroch";
+            alert("cockroch");
+            break;
+        case '3':
+            object2 = "nuclear bomb";
+            alert("nuclear bomb");
+            break;
+    }
+    if (object1 == "human" && object2 == "cockroch") {
+        document.write("human survives");
+        return "human survives";
+    } else if (object1 == "cockroch" && object2 == "human") {
+        document.write("human survives");
+        return "human survives";
+    } else if (object1 == "cockroch" && object2 == "nuclear bomb") {
+        document.write("cockroch survives");
+        return "cockroch survives";
+    } else if (object1 == "nuclearbomb" && object2 == "cockroch") {
+        document.write("cockroch survives");
+        return "cockroch survives";
+    } else if (object1 == "human" && object2 == "nuclear bomb") {
+        document.write("human dies");
+        return "human dies";
+    } else if (object1 == "nuclear bomb" && object2 == "human") {
+        document.write("human dies");
+        return "human dies";
+    } else {
+        document.write("tie");
+        return "tie";
+    }
+} 
